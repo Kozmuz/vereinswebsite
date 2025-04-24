5359fe8f52390b52cfb00055d938c4666c066e97
Kosmas Mathiopoulos <22mako1bdi1@hft-stuttgart.de>
2025-04-24 08:40:25 +0200

    Hinzufügen der PostgreSQL Konfiguration und anderer Änderungen

--- main/forms.py
+++ main/forms.py
@@ -6,7 +6,6 @@ class Anmeldeformular(forms.ModelForm):
         model = Anmeldung
         fields = ['vorname', 'nachname', 'email', 'bemerkung', 'termin', 'fahrzeugtyp', 'bezahlmethode']
     
-    # Optional: Du kannst hier noch einige Anpassungen machen, z.B. für bestimmte Felder ein Widget hinzufügen
     termin = forms.DateField(widget=forms.SelectDateWidget(years=range(2023, 2030)))  # Kalender für Termin
     fahrzeugtyp = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Fahrzeugtyp'}))
     bezahlmethode = forms.ChoiceField(

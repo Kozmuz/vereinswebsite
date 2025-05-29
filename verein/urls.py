60eede3ee96382300bcf49bae611164d3a2c5b3a
Kosmas Mathiopoulos <22mako1bdi1@hft-stuttgart.de>
2025-05-29 11:11:39 +0200

    Paypal more Fixes

--- verein/urls.py
+++ verein/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),  # ← Verbindet die App mit dem Projekt
+    path('paypal/', include('payments.urls')),
 ]

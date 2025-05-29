5c8c546db937fa5143fca52dc2b23438b1b2bd66
Kosmas Mathiopoulos <22mako1bdi1@hft-stuttgart.de>
2025-05-29 11:25:02 +0200

    Paypal more Fixes

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.anmeldung_view, name='anmeldung'),
+    path('anmeldung/', views.anmeldung_view, name='anmeldung'),
     path('paypal/create-order/', views.create_order, name='create_order'),
     path('paypal/capture-order/', views.capture_order, name='capture_order'),
     path('anmeldung-erfolgreich/', views.anmeldung_erfolg_view, name='anmeldung_erfolg'),
